daafd81bf6f88b50eb99aac6728152f79ced4c4d
lgirardet <122446334+lgirardet@users.noreply.github.com>
2023-01-17 22:44:22 +0100

    Add files via upload

---
+++ Serveur TCP.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Fprintf(conn, "Hello, client!\n")
+}
