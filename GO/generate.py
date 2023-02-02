d027b2a794de1c3dfc910951f3b35e648648dd3e
Shawn.S <93225931+zikangS@users.noreply.github.com>
2023-02-02 22:26:04 +0100

    Update generate.py

--- GO/generate.py
+++ GO/generate.py
@@ -1,12 +1,14 @@
 import random
 
+matrix_size=1000
+file_name="Mat.txt"
 
-for j in range(1000) :
+for j in range(matrix_size) :
 	row=""
-	for i in range(999) :
+	for i in range(matrix_size-1) :
 		row+=str(random.randint(0, 100))+" "
 	
 	row+=str(random.randint(0, 100))
-	with open("n2.txt", "a") as file:
+	with open(file_name, "a") as file:
 		file.write(row+"\n")
 
